fa6e7935734d5651293ad19ab7ad76e6fb0e432b
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-10-08 17:07:25 +0600

    mileS-9 cart shared to the two component

--- milestone-9-react-route-ema-john/src/components/Orders/Orders.jsx
+++ milestone-9-react-route-ema-john/src/components/Orders/Orders.jsx
@@ -3,19 +3,19 @@ import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
 
 const Orders = () => {
-  const products = useLoaderData();
-  console.log(products);
+  const cart = useLoaderData();
+  // console.log(products);
   return (
     <div>
       <div>
-        <h1>Order page: {products.length} </h1>
+        <h1>Order page: {cart.length} </h1>
       </div>
       <div className="px-4 bg-fuchsia-900 mx-4 w-1/4 leading-normal fixed top-0 right-0 mt-16">
         <h3 className="text-center font-bold text-xl py-4">Cart Container</h3>
         <h4 className="text-center font-bold text-md ">
-          Product Added: {products.length}
+          Product Added: {cart.length}
         </h4>
-        <Cart addCart={[]}></Cart>
+        <Cart addCart={cart}></Cart>
       </div>
     </div>
   );
